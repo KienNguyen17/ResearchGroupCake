705e13a97cd49b6a8e664299f3b696881b528272
Kien Nguyen <knguyen2@macalester.edu>
2025-03-31 23:25:45 -0500

    Documentation

--- wugCake.py
+++ wugCake.py
@@ -18,9 +18,9 @@ class BasicGui:
             relief=tk.RAISED,
             bg="#f3eed5"
         )
-        # mainCanvas.pack()
         self.mainCanvas.grid(row=0, column=0, columnspan=4)
 
+        # Button will fetch wugEntry's input, convert to int and feed to addWud function
         self.submitButton = tk.Button(self.rootWin,
             text="Set Wug Number",
             font = "Times 12",
@@ -29,7 +29,6 @@ class BasicGui:
             bd = 10,
             command= lambda : self.addWug(int(self.wugEntry.get())) 
         )
-        
         self.submitButton.grid(row = 1, column = 2)
 
         self.wugEntry = tk.Entry(self.rootWin)
@@ -41,14 +40,19 @@ class BasicGui:
         )
         self.wugEntry.grid(row = 1, column = 1)
 
+        # Convert to Image object
         self.photo = Image.open("./img/wug.png").resize((90, 110))
+
+        # List to contain ImageTk object, or else it gets deleted by garbage collector
         self.wugList = []
 
     def addWug(self, num):
+        # Reset the list
         self.wugList = []
         for i in range(num):
             self.wugList.append(ImageTk.PhotoImage(self.photo))
 
+        # Randomly generate the coordinates for new wug images
         for (j, wug) in enumerate(self.wugList):
             self.mainCanvas.create_image(random.randrange(0,400), random.randrange(0,300), image=wug)
 
