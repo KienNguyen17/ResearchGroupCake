ea2ddecb61e925254db94a5e7da7525b491e37a8
Kien Nguyen <knguyen2@macalester.edu>
2025-04-07 23:46:42 -0500

    added coord to wug

--- wugCake.py
+++ wugCake.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import random
+from wugFunction import *
 
 #Hi Kien, Here is Runchen
 
@@ -9,11 +10,13 @@ class BasicGui:
     def __init__(self):
         self.rootWin = tk.Tk()
         self.rootWin.title("Happy Birthday!")
+        self.HEIGHT = 600
+        self.WIDTH = 800
 
         self.mainCanvas = tk.Canvas(self.rootWin)
         self.mainCanvas.config(
-            width=400,
-            height=300,
+            width=self.WIDTH,
+            height=self.HEIGHT,
             bd=2,
             relief=tk.RAISED,
             bg="#f3eed5"
@@ -41,20 +44,77 @@ class BasicGui:
         self.wugEntry.grid(row = 1, column = 1)
 
         # Convert to Image object
-        self.photo = Image.open("./img/wug.png").resize((90, 110))
+        # self.photo = Image.open("./img/wug.png").resize((90, 110))
 
-        # List to contain ImageTk object, or else it gets deleted by garbage collector
-        self.wugList = []
+        # # List to contain ImageTk object, or else it gets deleted by garbage collector
+        # self.wugList = []
 
     def addWug(self, num):
+        self.mainCanvas.delete(tk.ALL)
         # Reset the list
-        self.wugList = []
+        # self.wugList = []
         for i in range(num):
-            self.wugList.append(ImageTk.PhotoImage(self.photo))
+        #     self.wugList.append(ImageTk.PhotoImage(self.photo))
 
-        # Randomly generate the coordinates for new wug images
-        for (j, wug) in enumerate(self.wugList):
-            self.mainCanvas.create_image(random.randrange(0,400), random.randrange(0,300), image=wug)
+        # # Randomly generate the coordinates for new wug images
+        # for (j, wug) in enumerate(self.wugList):
+            # self.mainCanvas.create_image(random.randrange(0,400), random.randrange(0,300), image=wug)
+            self.addOneWug(random.randrange(0,self.WIDTH), random.randrange(0,self.HEIGHT))
+            
+    def addOneWug(self, x, y):
+        x1, y1 = 10, 0
+        x2, y2 = 40, 0
+        x3, y3 = 0, 25
+        x4, y4 = 60, 17
+        points1 = bezier_curve(x1, y1, x2, y2, x3, y3, x4, y4, tk=True)
+
+        # Second curve control points
+        a1, b1 = 10, 0
+        a2, b2 = 80, -5
+        a3, b3 = 38, 17
+        a4, b4 = 60, 17
+        points2 = bezier_curve(a1, b1, a2, b2, a3, b3, a4, b4, tk=True)
+
+        #leg left
+        i1, j1 = 33, 1
+        i2, j2 = 32, -3
+        i3, j3 = 30, -4
+        i4, j4 = 29, -4.5
+        points3 = bezier_curve(i1, j1, i2, j2, i3, j3, i4, j4, tk=True)
+
+        #leg right
+        h1, k1 = 45, 0.5
+        h2, k2 = 46, -1
+        h3, k3 = 47, -3.5
+        h4, k4 = 45, -4.5
+        points4 = bezier_curve(h1, k1, h2, k2, h3, k3, h4, k4, tk=True)
+
+        #leg foot
+        q1, p1 = 26.5, -4.5
+        q2, p2 = 29, -4.6
+        q3, p3 = 33, -4
+        q4, p4 = 35, -4.5
+        points5 = bezier_curve(q1, p1, q2, p2, q3, p3, q4, p4, tk=True)
+
+        #right foot
+        w1, z1 = 45, -4.5
+        w2, z2 = 48, -4.5
+        w3, z3 = 52, -4.3
+        w4, z4 = 53, -4.5
+        points6 = bezier_curve(w1, z1, w2, z2, w3, z3, w4, z4, tk=True)
+
+        # self.mainCanvas.create_line(*(points1 + points2 + points3 + points4 + points5 + points6 ), fill = "black", smooth = True, width = 4)
+        self.mainCanvas.create_polygon(*(points1 + points2), fill = "lightblue", smooth = True, width = 3, tags=("wug" + str(x) + str(y)))
+        # self.mainCanvas.create_line(*(points2), fill = "blue", smooth = True, width = 3, tags=("wug" + str(x) + str(y)))
+        self.mainCanvas.create_line(*(points3), fill = "blue", smooth = True, width = 3, tags=("wug" + str(x) + str(y)))
+        self.mainCanvas.create_line(*(points4), fill = "blue", smooth = True, width = 3, tags=("wug" + str(x) + str(y)))
+        self.mainCanvas.create_line(*(points5), fill = "blue", smooth = True, width = 3, tags=("wug" + str(x) + str(y)))
+        self.mainCanvas.create_line(*(points6), fill = "blue", smooth = True, width = 3, tags=("wug" + str(x) + str(y)))
+
+        self.mainCanvas.create_oval(-50, -18, -44, -10, fill="black", tags=("wug" + str(x) + str(y)))
+        # self.mainCanvas.move(self.mainCanvas.find_withtag("wug"), 100, 100)
+        for i in self.mainCanvas.find_withtag("wug" + str(x) + str(y)):
+            self.mainCanvas.move(i, x, y)
 
     def run(self):
         self.rootWin.mainloop()
--- wugFunction.py
+++ wugFunction.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def bezier_curve(x1, y1, x2, y2, x3, y3, x4, y4):
+def bezier_curve(x1, y1, x2, y2, x3, y3, x4, y4, tk=False):
     """
     Calculates points on a cubic Bézier curve.
 
@@ -18,80 +18,86 @@ def bezier_curve(x1, y1, x2, y2, x3, y3, x4, y4):
     t_values = np.linspace(0, 1, 100)  # Generate 100 t values
     x_values = []
     y_values = []
+    tk_values = []
 
     for t in t_values:
         x = (1-t)**3*x1 + 3*(1-t)**2*t*x2 + 3*(1-t)*t**2*x3 + t**3*x4
         y = (1-t)**3*y1 + 3*(1-t)**2*t*y2 + 3*(1-t)*t**2*y3 + t**3*y4
         x_values.append(x)
         y_values.append(y)
-
+        tk_values.append(-x)
+        tk_values.append(-y)
+    if tk:
+        return tk_values
     return x_values, y_values
 
-# Example Usage
-x1, y1 = 10, 0
-x2, y2 = 40, 0
-x3, y3 = 0, 25
-x4, y4 = 60, 17
-x_points1, y_points1 = bezier_curve(x1, y1, x2, y2, x3, y3, x4, y4)
 
-# Second curve control points
-a1, b1 = 10, 0
-a2, b2 = 80, -5
-a3, b3 = 38, 17
-a4, b4 = 60, 17
-x_points2, y_points2 = bezier_curve(a1, b1, a2, b2, a3, b3, a4, b4)
+if __name__ == "__main__":
+    # Example Usage
+    x1, y1 = 10, 0
+    x2, y2 = 40, 0
+    x3, y3 = 0, 25
+    x4, y4 = 60, 17
+    x_points1, y_points1 = bezier_curve(x1, y1, x2, y2, x3, y3, x4, y4)
 
-#leg left
-i1, j1 = 33, 1
-i2, j2 = 32, -3
-i3, j3 = 30, -4
-i4, j4 = 29, -4.5
-x_points3, y_points3 = bezier_curve(i1, j1, i2, j2, i3, j3, i4, j4)
+    # Second curve control points
+    a1, b1 = 10, 0
+    a2, b2 = 80, -5
+    a3, b3 = 38, 17
+    a4, b4 = 60, 17
+    x_points2, y_points2 = bezier_curve(a1, b1, a2, b2, a3, b3, a4, b4)
 
-#leg right
-h1, k1 = 45, 0.5
-h2, k2 = 46, -1
-h3, k3 = 47, -3.5
-h4, k4 = 45, -4.5
-x_points4, y_points4 = bezier_curve(h1, k1, h2, k2, h3, k3, h4, k4)
+    #leg left
+    i1, j1 = 33, 1
+    i2, j2 = 32, -3
+    i3, j3 = 30, -4
+    i4, j4 = 29, -4.5
+    x_points3, y_points3 = bezier_curve(i1, j1, i2, j2, i3, j3, i4, j4)
 
-#leg foot
-q1, p1 = 26.5, -4.5
-q2, p2 = 29, -4.6
-q3, p3 = 33, -4
-q4, p4 = 35, -4.5
-x_points5, y_points5 = bezier_curve(q1, p1, q2, p2, q3, p3, q4, p4)
+    #leg right
+    h1, k1 = 45, 0.5
+    h2, k2 = 46, -1
+    h3, k3 = 47, -3.5
+    h4, k4 = 45, -4.5
+    x_points4, y_points4 = bezier_curve(h1, k1, h2, k2, h3, k3, h4, k4)
 
-#right foot
-w1, z1 = 45, -4.5
-w2, z2 = 48, -4.5
-w3, z3 = 52, -4.3
-w4, z4 = 53, -4.5
-x_points6, y_points6 = bezier_curve(w1, z1, w2, z2, w3, z3, w4, z4)
+    #leg foot
+    q1, p1 = 26.5, -4.5
+    q2, p2 = 29, -4.6
+    q3, p3 = 33, -4
+    q4, p4 = 35, -4.5
+    x_points5, y_points5 = bezier_curve(q1, p1, q2, p2, q3, p3, q4, p4)
 
-
-# Plot the curve
-plt.plot(x_points1, y_points1, label='Curve 1', color='blue')
-plt.plot(x_points2, y_points2, label='Curve 2', color='green')
-plt.plot(x_points3, y_points3, label='Leg Left', color='purple')
-plt.plot(x_points4, y_points4, label='Leg Right', color='navy')
-plt.plot(x_points5, y_points5, label='Leg Right', color='blueviolet')
-plt.plot(x_points6, y_points6, label='Leg Right', color='royalblue')
-plt.scatter([x1, x2, x3, x4], [y1, y2, y3, y4], color='red', marker='o')  # Plot control
-plt.scatter([a1, a2, a3, a4], [b1, b2, b3, b4], color='orange', label='Curve 2 Control Points')
-plt.scatter([i1, i2, i3, i4], [j1, j2, j3, j4], color='pink', marker='o')
-plt.scatter([h1, h2, h3, h4], [k1, k2, k3, k4], color='grey', marker='o') # Plot control
-plt.scatter([q1, q2, q3, q4], [p1, p2, p3, p4], color='blueviolet', label='o')
-plt.scatter([w1, w2, w3, w4], [z1, z2, z3, z4], color='royalblue', label='o')
+    #right foot
+    w1, z1 = 45, -4.5
+    w2, z2 = 48, -4.5
+    w3, z3 = 52, -4.3
+    w4, z4 = 53, -4.5
+    x_points6, y_points6 = bezier_curve(w1, z1, w2, z2, w3, z3, w4, z4)
 
 
-# Add solid circle
-circle = plt.Circle((45, 13), 1, color='black', alpha=1)  # Circle with center (40, 30) and radius 15
-plt.gca().add_patch(circle)
+    # Plot the curve
+    plt.plot(x_points1, y_points1, label='Curve 1', color='blue')
+    plt.plot(x_points2, y_points2, label='Curve 2', color='green')
+    plt.plot(x_points3, y_points3, label='Leg Left', color='purple')
+    plt.plot(x_points4, y_points4, label='Leg Right', color='navy')
+    plt.plot(x_points5, y_points5, label='Leg Right', color='blueviolet')
+    plt.plot(x_points6, y_points6, label='Leg Right', color='royalblue')
+    plt.scatter([x1, x2, x3, x4], [y1, y2, y3, y4], color='red', marker='o')  # Plot control
+    plt.scatter([a1, a2, a3, a4], [b1, b2, b3, b4], color='orange', label='Curve 2 Control Points')
+    plt.scatter([i1, i2, i3, i4], [j1, j2, j3, j4], color='pink', marker='o')
+    plt.scatter([h1, h2, h3, h4], [k1, k2, k3, k4], color='grey', marker='o') # Plot control
+    plt.scatter([q1, q2, q3, q4], [p1, p2, p3, p4], color='blueviolet', label='o')
+    plt.scatter([w1, w2, w3, w4], [z1, z2, z3, z4], color='royalblue', label='o')
 
-# points
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.title("Cubic Bézier Curve")
-plt.grid(True)
-plt.show()
+
+    # Add solid circle
+    circle = plt.Circle((45, 13), 1, color='black', alpha=1)  # Circle with center (40, 30) and radius 15
+    plt.gca().add_patch(circle)
+
+    # points
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title("Cubic Bézier Curve")
+    plt.grid(True)
+    plt.show()
